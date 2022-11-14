385f49a8809dc350b564813d39110aca1262205f
Justin Navarro <info@justinnavarro.net>
2022-11-14 06:12:57 -0500

    fixed tabs

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-	<v-app>
-		<v-app-bar app dark>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Heroes</span>
-			</v-toolbar-title>
-		</v-app-bar>
-
-		<v-content style="margin-bottom: 2rem">
-			Pizza
-		</v-content>
+	<v-app style="background: #f9f9f9">
+		<v-container>
+			<v-card>
+				<v-card-title :style="{ color: this.$vuetify.theme.themes.light.primary }">1. Add Vehicles</v-card-title>
+				<v-tabs grow>
+					<v-tab>Single Vin</v-tab>
+					<v-tab>Bulk Vin</v-tab>
+					<v-tab>Manual Entry</v-tab>
+				</v-tabs>
+			</v-card>
+		</v-container>
 	</v-app>
 </template>
 
